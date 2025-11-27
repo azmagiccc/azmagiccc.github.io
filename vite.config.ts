51a1f6981e8fc394bb5807a759829528e921ff4d
azmagiccc <azmagiccc@gmail.com>
2025-11-27 20:11:25 +0800

    base

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // use relative assets so it works on GitHub Pages or custom domain roots
   plugins: [react()],
 })
